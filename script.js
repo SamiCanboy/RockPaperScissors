f0702171b0bd58adc1efa8ad3a2da8d744dd777f
Sami Canboy <68285862+SamiCanboy@users.noreply.github.com>
2024-11-18 14:11:14 +0000

    x

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let headerdiv = document.createElement('header')
+document.body.style.backgroundColor = 'orange'
+
 let title = document.createElement('h2')
-todo.innerHTML = 'RockPaperScissors'
+let result = document.createElement('h1')
+let rockButton = document.createElement('button')
+let paperButton = document.createElement('button')
+let scissorsButton = document.createElement('button')
+
+
+
+title.innerHTML = "RockPaperScissors"
+title.style.textAlign = 'center'
+result.innerHTML = 'Oyuna Başla'
+
+
+
+document.body.appendChild(title)
+document.body.appendChild(rockimage)
+document.body.appendChild(result)
+document.body.appendChild(rockButton)
+document.body.appendChild(paperButton)
+document.body.appendChild()
